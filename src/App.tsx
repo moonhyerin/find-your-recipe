b99ef709655ee18a1990f1d7c2bb486ea64a7c55
unknown <hrmoon081632@gmail.com>
2023-05-26 12:48:31 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Test</h1>
+        <div>home</div>
+        <div>recipe</div>
+        <div>about</div>
+      </div>
+      <div>searchbar</div>
+    </div>
+  );
+}
+
+export default App;
