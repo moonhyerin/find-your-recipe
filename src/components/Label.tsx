981fd020c27b2e2f2ef06cbc6faa7048d265472d
unknown <hrmoon081632@gmail.com>
2023-06-25 21:47:02 +0200

    fix: change property for custom style

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
 
 type PropsType = {
-  paramKey?: string;
   value: string;
+  clickable?: boolean;
+  paramKey?: string;
+  customStyle?: string;
   handleChecked?: (key: string, value: string) => void;
 };
 
-function Label({ paramKey, value, handleChecked }: PropsType) {
+function Label({
+  clickable,
+  paramKey,
+  value,
+  customStyle,
+  handleChecked,
+}: PropsType) {
   const [checked, setChecked] = useState(false);
 
   const handleClick = () => {
-    setChecked(!checked);
-
-    if (paramKey && handleChecked) handleChecked(paramKey, value);
+    if (paramKey && handleChecked) {
+      setChecked(!checked);
+      handleChecked(paramKey, value);
+    }
   };
+
+  const style = clickable
+    ? `bg-slate-400/10 text-slate-500 cursor-pointer ${customStyle}`
+    : `!bg-[#ff512e] text-white ${customStyle}`;
 
   return (
     <div
-      className={`text-xs inline-block mr-2 my-1 py-2 px-2 rounded-full cursor-pointer ${
-        checked ? 'bg-[#ff512e] text-white' : 'bg-slate-400/10 text-slate-500'
+      className={`text-xs inline-block rounded-full ${style} ${
+        checked && '!bg-[#ff512e] !text-white'
       }`}
       onClick={handleClick}
     >
