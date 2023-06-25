49df5d79962fd5d602936b1510a7a6a0afc67529
unknown <hrmoon081632@gmail.com>
2023-06-25 21:45:14 +0200

    fix: separate types

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,48 @@ type ResultType = {
   nutrition: Object;
 };
 
-export type { CategoryType, ResultType };
+type AnalyzedInstructionType = {
+  name: string;
+  steps: [];
+};
+
+type RandomRecipeType = {
+  aggregateLikes: number;
+  analyzedInstructions: AnalyzedInstructionType[];
+  cheap: boolean;
+  cookingMinutes: number;
+  creditsText: string;
+  cuisines: [];
+  dairyFree: boolean;
+  diets: string[];
+  dishTypes: string[];
+  extendedIngredients: [];
+  gaps: string;
+  glutenFree: boolean;
+  healthScore: number;
+  id: number;
+  image: string;
+  imageType: string;
+  instructions: string;
+  license: string;
+  lowFodmap: boolean;
+  occasions: [];
+  originalId: number | null;
+  preparationMinutes: number;
+  pricePerServing: number;
+  readyInMinutes: number;
+  servings: number;
+  sourceName: string;
+  sourceUrl: string;
+  spoonacularSourceUrl: string;
+  summary: string;
+  sustainable: boolean;
+  title: string;
+  vegan: boolean;
+  vegetarian: boolean;
+  veryHealthy: boolean;
+  veryPopular: boolean;
+  weightWatcherSmartPoints: number;
+};
+
+export type { CategoryType, ResultType, RandomRecipeType };
