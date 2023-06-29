aa3b899cd138cb4bf0ecb24fe2f5ef07ef2ae41e
unknown <hrmoon081632@gmail.com>
2023-06-29 15:29:05 +0200

    fix: change css of searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,7 +21,7 @@ function SearchBar(props: PropsType) {
       <SearchIcon sx={{ width: 20, height: 20 }} />
       <input
         type='text'
-        className='ml-2 w-full bg-transparent border-none focus:outline-none'
+        className='font-inter tracking-[1.2] ml-2 w-full bg-transparent border-none focus:outline-none'
         placeholder={placeholder}
         value={value}
         onChange={onChange}
