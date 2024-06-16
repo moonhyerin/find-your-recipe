176c97591dea0227f2968249f6b8dab90f33018b
unknown <hrmoon081632@gmail.com>
2024-06-16 21:43:50 +0200

    WIP: add content of about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import BaseSection from '../components/BaseSection';
 
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <BaseSection>
+      <div className='mb-5'>
+        <h2 className='text-lg md:text-xl font-lora font-semibold mb-3 underline decoration-2 decoration-[#ff512e]'>
+          Created by
+        </h2>
+        <p className='mb-2 text-base font-inter font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
+          Hyerin Mun
+        </p>
+      </div>
+      <div className='mb-5'>
+        <h2 className='text-lg md:text-xl font-lora font-semibold mb-3 underline decoration-2 decoration-[#ff512e]'>
+          References
+        </h2>
+        <p className='mb-2 text-base font-inter font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
+          Recipe from Spoonacular api (
+          <a
+            href='https://spoonacular.com/food-api'
+            className='hover:underline hover:decoration-2 hover:decoration-[#ff512e]'
+          >
+            Link
+          </a>
+          )
+        </p>
+      </div>
+    </BaseSection>
+  );
 }
 
 export default AboutPage;
